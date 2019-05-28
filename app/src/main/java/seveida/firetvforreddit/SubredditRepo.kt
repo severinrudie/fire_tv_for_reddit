390c46bcebaced12ac420341e0fa73e40a400223
Severin Rudie <srudie@mozilla.com>
2019-05-27 20:31:01 -0700

    Cleaned up SubredditRepo Kt conversion

--- app/src/main/java/seveida/firetvforreddit/SubredditRepo.kt
+++ app/src/main/java/seveida/firetvforreddit/SubredditRepo.kt
@@ -5,19 +5,16 @@ import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Path
 import seveida.firetvforreddit.domain.objects.SubredditDetails
+import seveida.firetvforreddit.domain.objects.toSubredditDetails
 import seveida.firetvforreddit.response.objects.SubredditResponse
 
 class SubredditRepo internal constructor(retrofit: Retrofit) {
 
-    private val api: SubredditApi
-
-    init {
-        api = retrofit.create(SubredditApi::class.java)
-    }
+    private val api: SubredditApi = retrofit.create(SubredditApi::class.java)
 
     fun getSubreddit(subreddit: String): Observable<SubredditDetails> {
         return api.getSubreddit(subreddit)
-            .map { response -> SubredditDetails.fromResponse(response) }
+            .map { it.toSubredditDetails() }
     }
 }
 
