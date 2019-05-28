6becdc7cd455c1e56d3007d304a6d5f254536c82
Severin Rudie <srudie@mozilla.com>
2019-05-27 20:31:18 -0700

    Automatically converted SubredditRepoTest to Kt
    No manual touchup done

---
+++ app/src/test/java/seveida/firetvforreddit/SubredditRepoTest.kt
@@ -0,0 +1,27 @@
+package seveida.firetvforreddit
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class SubredditRepoTest {
+
+    @Test
+    fun t() {
+        val retrofit = RetrofitProvider.create()
+
+        val repo = SubredditRepo(retrofit)
+
+        repo.getSubreddit("boobs")
+            .subscribe { (_, threadMetadataList) -> threadMetadataList.forEach { (_, _, title) -> println(title) } }
+
+        try {
+            Thread.sleep(10000)
+        } catch (e: Exception) {
+
+        }
+
+    }
+
+}
