0dd884cda07651b52bb539869244a92e374ec114
Severin Rudie <srudie@mozilla.com>
2019-05-27 21:02:31 -0700

    Added blank subreddit fragment

---
+++ app/src/main/java/seveida/firetvforreddit/subreddit/SubredditFragment.kt
@@ -0,0 +1,18 @@
+package seveida.firetvforreddit.subreddit
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import seveida.firetvforreddit.R
+
+class SubredditFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_subreddit, container, false)
+
+
+}
