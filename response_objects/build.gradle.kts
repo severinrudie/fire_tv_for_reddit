4733d6efd47d69e42f358d1e937db3429a5daeb0
THOR\Severin <severinrudie@gmail.com>
2019-05-28 22:43:45 -0700

    Added domain and response modules

---
+++ response_objects/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android-extensions")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(BuildConstants.compileSdkVersion)
+    defaultConfig {
+        minSdkVersion(BuildConstants.minSdkVersion)
+        targetSdkVersion(BuildConstants.targetSdkVersion)
+        versionCode = BuildConstants.versionCode
+        versionName = BuildConstants.versionName
+        testInstrumentationRunner = BuildConstants.testInstrumentationRunner
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = BuildConstants.minifyEnabled
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(Dependencies.kotlin_stdlib)
+
+    implementation(Dependencies.appCompat)
+
+    testImplementation(Dependencies.robolectric)
+    testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.testRunner)
+    androidTestImplementation(Dependencies.espressoCore)
+}
