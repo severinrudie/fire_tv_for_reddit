c707534e6b91078cae2bef875898656955e2086d
THOR\Severin <severinrudie@gmail.com>
2019-05-29 19:25:05 -0700

    Added io module

---
+++ io/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android-extensions")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(BuildConstants.compileSdkVersion)
+    defaultConfig {
+        minSdkVersion(BuildConstants.minSdkVersion)
+        targetSdkVersion(BuildConstants.targetSdkVersion)
+        versionCode = BuildConstants.versionCode
+        versionName = BuildConstants.versionName
+        testInstrumentationRunner = BuildConstants.testInstrumentationRunner
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = BuildConstants.minifyEnabled
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(project(Dependencies.domain_objects))
+    implementation(project(Dependencies.response_objects))
+
+    implementation(Dependencies.kotlin_stdlib)
+
+    implementation(Dependencies.appCompat)
+
+    implementation(Dependencies.rxJava)
+    implementation(Dependencies.rxAndroid)
+    implementation(Dependencies.rxKotlin)
+
+    implementation(Dependencies.okhttp)
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.retrofitRxAdapter)
+    implementation(Dependencies.moshi)
+    implementation(Dependencies.retrofitMoshiAdapter)
+    kapt (Dependencies.moshiKapt)
+
+    // Java Time backport
+    implementation(Dependencies.threeTenAndroid)
+    // Allows the time backport to work during testing
+    testImplementation(Dependencies.threeTenJava)
+
+    testImplementation(Dependencies.robolectric)
+    testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.testRunner)
+    androidTestImplementation(Dependencies.espressoCore)
+}
