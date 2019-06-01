6e3d09ae2eee8ec870878a54d131da55d40cb972
THOR\Severin <severinrudie@gmail.com>
2019-06-01 12:16:07 -0700

    Added TextInputAutoCompleteTextView

--- app/src/main/java/seveida/firetvforreddit/view/components/TextInputAutoCompleteTextView.kt
+++ app/src/main/java/seveida/firetvforreddit/view/components/TextInputAutoCompleteTextView.kt
@@ -1,2 +1,32 @@
 package seveida.firetvforreddit.view.components
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.inputmethod.InputConnection
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
+
+/**
+ * An [AppCompatAutoCompleteTextView] that can be used inside of a [TextInputLayout]
+ *
+ * Code taken from https://stackoverflow.com/a/41864063
+ */
+class TextInputAutoCompleteTextView : AppCompatAutoCompleteTextView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onCreateInputConnection(outAttrs: EditorInfo): InputConnection? {
+        val ic = super.onCreateInputConnection(outAttrs)
+        if (ic != null && outAttrs.hintText == null) {
+            // If we don't have a hint and our parent is a TextInputLayout, use it's hint for the
+            // EditorInfo. This allows us to display a hint in 'extract mode'.
+            val parent = parent
+            if (parent is TextInputLayout) {
+                outAttrs.hintText = parent.hint
+            }
+        }
+        return ic
+    }
+}
